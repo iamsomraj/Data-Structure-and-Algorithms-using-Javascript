6c06ab4247f329b6bfed468f937c7ad05b6032c7
iamsomraj <iamsomraj@gmail.com>
2020-09-22 12:08:27 +0530

    string reverse recursion

---
+++ Recursion Examples/String Reverse/app.js
@@ -0,0 +1,11 @@
+const rev = (string) => {
+	if (string.length === 1) {
+		return string;
+	}
+	return (
+		string.charAt(string.length - 1) +
+		rev(string.substring(0, string.length - 1))
+	);
+};
+
+console.log(rev("Somraj"));
